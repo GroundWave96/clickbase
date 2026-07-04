8dd0d57abbb9f95e0dae6d235f574750831fcad4
GroundWave96 <gabrielhfp96@gmail.com>
2026-07-04 16:53:37 -0300

    feat: adiciona botão de login do google e corrige tsconfig

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    google.accounts.id.initialize({
+      client_id: '133533206595-81ufmjk6israq9pepoittj21o7d9ofeh.apps.googleusercontent.com',
+      callback: this.lidarComLogin.bind(this)
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('google-btn'),
+      { theme: 'outline', size: 'large', shape: 'pill' }
+    );
+  }
+
+  lidarComLogin(response: any) {
+    console.log('JWT recebido do Google:', response.credential);
+  }
+}
